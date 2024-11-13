d060c1d4a268cbf4dced31a068a0588a5ce1b9fe
Arman Rajib <ahrajib6789@gmail.com>
2024-11-13 14:38:51 +0600

    Add useReducer hook & implement fake Authentication

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,49 +1,98 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
 const CitiesContext = createContext();
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+
+    case "cities/loaded":
+      return { ...state, isLoading: false, cities: action.payload };
+
+    case "city/loaded":
+      return { ...state, isLoading: false, currentCity: action.payload };
+
+    case "city/created":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
+
+    case "rejected":
+      return { ...state, isLoading: false, error: action.payload };
+
+    default:
+      throw new Error(`Action type ${action.type} is not supported!`);
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsloading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(function () {
-    setIsloading(true);
+    dispatch({ type: "loading" });
+
     async function fetchCities() {
       try {
-        setIsloading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+        dispatch({ type: "cities/loaded", payload: data });
         console.log(data);
       } catch {
-        alert("Some error occured!");
-      } finally {
-        setIsloading(false);
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading cities!",
+        });
       }
     }
     fetchCities();
   }, []);
 
   async function getCities(id) {
+    // If the city is already loaded, don't fetch it again
+    if (Number(id) === currentCity.id) return;
+
+    dispatch({ type: "loading" });
+
     try {
-      setIsloading(true);
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: "city/loaded", payload: data });
     } catch {
-      alert("There was an error loading city!");
-    } finally {
-      setIsloading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was an error loading city!",
+      });
     }
   }
 
   async function createCity(newCity) {
-    try {
-      setIsloading(true);
+    dispatch({ type: "loading" });
 
+    try {
       const res = await fetch(`${BASE_URL}/cities`, {
         method: "POST",
         headers: {
@@ -55,23 +104,26 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       console.log(data);
 
-      setCities([...cities, data]);
+      dispatch({ type: "city/created", payload: data });
     } catch {
-      alert("There was an error creating city!");
-    } finally {
-      setIsloading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was an error creating city!",
+      });
     }
   }
 
   async function deleteCity(id) {
+    dispatch({ type: "loading" });
+
     try {
-      setIsloading(true);
       await fetch(`${BASE_URL}/cities/${id}`, { method: "DELETE" });
-      setCities((cities) => cities.filter((city) => city.id !== id));
+      dispatch({ type: "city/deleted", payload: id });
     } catch {
-      alert("There was an error deleting city!");
-    } finally {
-      setIsloading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was an error deleting city!",
+      });
     }
   }
 
@@ -81,6 +133,7 @@ function CitiesProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        error,
         getCities,
         createCity,
         deleteCity,
---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,61 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, isAuthenticated: true, user: action.payload };
+
+    case "logout":
+      return { ...state, isAuthenticated: false, user: null };
+
+    default:
+      throw new Error(`Action type ${action.type} is not supported!`);
+  }
+}
+
+function AuthProvider({ children }) {
+  const [{ isAuthenticated, user }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
